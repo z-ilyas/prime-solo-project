ef94f8e1bfde81030295faebaf8652d5967354e4
Ilyas Mohamed <zswaleh@ymail.com>
2023-06-02 22:05:28 -0500

    fixed a bug causing user unable to create previous exercises

--- src/components/CreateExercise/CreateExercise.jsx
+++ src/components/CreateExercise/CreateExercise.jsx
@@ -1,37 +1,56 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { useHistory } from 'react-router-dom';
 import Swal from "sweetalert2";
 
 function CreateExercise() {
-    
+    const [previous, setPrevious] = useState('');
     const [name, setName] = useState('');
     const [date, setDate] = useState('');
     const dispatch = useDispatch();
     const history = useHistory();
     const completedExercises = useSelector((store) => store.completedExercisesReducer);
 
-
     const createExercise = () => {
-        if(name === '' || date === '' ){
-            Swal.fire({
-                icon: 'error',
-                title: 'PLEASE FILL IN ALL THE INPUTS',
-              })
-        }
-        else{
-        dispatch({ 
-            type: 'SAGA_CREATE_EXERCISE',
-            payload:{
-                name: name,
-                date: date,
-                is_completed: false
+        if (previous === 'newExercise') {
+            if (name !== '') {
+                console.log("Dispatching SAGA_CREATE_EXERCISE action");
+                dispatch({
+                    type: 'SAGA_CREATE_EXERCISE',
+                    payload: {
+                        name: name,
+                        date: date,
+                        is_completed: false,
+                    },
+                });
+                setName('');
+            } else {
+                console.log("Fields are empty, displaying alert. Name:", name, "Date:", date);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'PLEASE FILL IN ALL THE INPUTS',
+                });
             }
-        });
-        setName('');
+        } else if (previous) {
+            console.log("Dispatching SAGA_CREATE_PREVIOUS_EXERCISE action");
+            dispatch({
+                type: 'SAGA_CREATE_PREVIOUS_EXERCISE',
+                payload: {
+                    name: previous,
+                    date: date,
+                    is_completed: false,
+                },
+            });
         }
-    }
+        setName('');
+    };
+    
+    // Add the following useEffect hook to check the state values after setting them
+    useEffect(() => {
+        console.log("Name after state update:", name);
+        console.log("Date after state update:", date);
+    }, [name, date]);
+
     const goToUserPage = () => {
         history.push('/user');
     }
@@ -39,7 +58,7 @@ function CreateExercise() {
     return(
         <div>
      <div className="create-exercise-container">
-        <select value={name} onChange={(event) => setName(event.target.value)}>
+        <select value={previous} onChange={(event) => setPrevious(event.target.value)}>
             <option>Drop Down</option>
             <option value="newExercise"> 
                 <p>Create New Exercise</p>
@@ -50,7 +69,7 @@ function CreateExercise() {
           </option>
           ))}
         </select>
-        {name === "newExercise" && (
+        {previous === "newExercise" && (
         <input
             className="name-input"
             placeholder='Create New Exercise'
